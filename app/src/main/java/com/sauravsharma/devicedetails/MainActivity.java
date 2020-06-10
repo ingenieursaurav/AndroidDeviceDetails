d0ff246c1f70336b5585ee3add075e2ec94bca18
Saurav Sharma <ingenieursaurav@gmail.com>
2020-06-10 15:42:46 +0530

    First Release

---
+++ app/src/main/java/com/sauravsharma/devicedetails/MainActivity.java
@@ -0,0 +1,45 @@
+package com.sauravsharma.devicedetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        data =findViewById(R.id.details);
+        String  details =  "VERSION.RELEASE : "+Build.VERSION.RELEASE
+                +"\nVERSION.INCREMENTAL : "+Build.VERSION.INCREMENTAL
+                +"\nVERSION.SDK.NUMBER : "+Build.VERSION.SDK_INT
+                +"\nBOARD : "+Build.BOARD
+                +"\nBOOTLOADER : "+Build.BOOTLOADER
+                +"\nBRAND : "+Build.BRAND
+                +"\nCPU_ABI : "+Build.CPU_ABI
+                +"\nCPU_ABI2 : "+Build.CPU_ABI2
+                +"\nDISPLAY : "+Build.DISPLAY
+                +"\nFINGERPRINT : "+Build.FINGERPRINT
+                +"\nHARDWARE : "+Build.HARDWARE
+                +"\nHOST : "+Build.HOST
+                +"\nID : "+Build.ID
+                +"\nMANUFACTURER : "+ Build.MANUFACTURER
+                +"\nMODEL : "+Build.MODEL
+                +"\nPRODUCT : "+Build.PRODUCT
+                +"\nSERIAL : "+Build.SERIAL
+                +"\nTAGS : "+Build.TAGS
+                +"\nTIME : "+Build.TIME
+                +"\nTYPE : "+Build.TYPE
+                +"\nUNKNOWN : "+Build.UNKNOWN
+                +"\nUSER : "+Build.USER;
+
+        Log.d("Device Details",details);
+        data.setText(details);
+    }
+}
